8a3088badee292d365989ee04d1df08d183f6459
Mudasir <muhammadmudasirbse138@gmail.com>
2023-12-21 19:12:37 -1000

    used max and min for lightness technique

--- colorUtils.c
+++ colorUtils.c
@@ -11,7 +11,37 @@ int max(int x, int y, int z) {
   return m;
 }
 
+int min (int x, int y, int z){
+  int m = x<y ? x:y;
+  m = m < z ? m:z;
+}
+
+
+
 int toGrayScaleAverage(int r, int g, int b) {
   //TODO: test this, it may be wrong!
   return (r + g + b) / 3;
+}
+int toGrayScaleLightness (int r, int g,int b)
+int max = 0, min = 0;
+if (r>g && r>b) {
+  max = r;
+}
+else if (g>r && g>b){
+  max = g;
+}
+else {
+  max = b;
+}
+if (r<g && r<b){
+  min = r;
+}
+else if (g<r && g<b){
+  min = g;
+}
+else {
+  min = b;
+}
+return (max + min) / 2;
+
 }
